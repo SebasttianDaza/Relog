767726dae1c1e88ac16fe40e3d9c24a4bdd65821
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-09-27 07:49:35 -0500

    feat(Compilation): Change eslint for module, and add alias to vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@Components": path.resolve(__dirname, "src/Components"),
+      "@Errors": path.resolve(__dirname, "src/Errors"),
+      "@Hooks": path.resolve(__dirname, "src/Hooks"),
+      "@Pages": path.resolve(__dirname, "src/Pages"),
+      "@Style-components": path.resolve(__dirname, "src/Style-components"),
+      "@Utils": path.resolve(__dirname, "src/Utils"),
+      "@Services": path.resolve(__dirname, "src/Services"),
+    },
+  },
+});
