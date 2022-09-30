2d7fe32b1068c36f30e8811f45847910a04e4c04
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-09-30 07:49:11 -0500

    feat(Pages): Add home404, and a new svg

--- src/Pages/Home404/home404.jsx
+++ src/Pages/Home404/home404.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { Center, Button, Flex } from "@chakra-ui/react";
 import { Title } from "@Components";
 
 const Home404 = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <Title text="Error 404" color="teal.500" />
+      <Center bg="blue" h="100vh">
+        <Flex flexDirection="column" alignItems="center">
+          <Title text="Not found 404" color="white" />
+          <Button colorScheme="teal" size="lg" mt="10" onClick={() => navigate("/")}>
+            Go Back
+          </Button>
+        </Flex>
+      </Center>
     </>
   );
 };
